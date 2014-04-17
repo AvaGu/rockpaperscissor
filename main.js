2c504e7a70dfdd72f4d458aef29f9d0eda914fd7
FanGu <ava.gu1990@gmail.com>
2014-04-16 22:24:56 -0500

    add modal

--- main.js
+++ main.js
@@ -1,5 +1,49 @@
-function clickImage(){
-	console.log("clickImage excuted!");
+
+function clickImage(choice){
+	// console.log("clickImage excuted!");
+	// console.log(choice);
+	var number = Math.floor(Math.random() * 3);
+	// console.log(number);
+	var computer = ["rock","paper","scissor"];
+	var result = getResult(choice, computer[number]);
+	if(result == 0){
+		console.log("Tie.");
+	}
+	else if(result == 1){
+		console.log("You win.");
+	}
+	else{
+		console.log("You lose.");
+	}
+
+	$('#myModal').modal('show');
+}
+
+function getResult(player, computer){
+	if(player == "rock"){
+		if(computer == "rock")
+			return 0;
+		else if(computer == "scissor")
+			return 1;
+		else if(computer == "paper")
+			return -1;
+	}
+	else if(player == "paper"){
+		if(computer == "rock")
+			return 1;
+		else if(computer == "scissor")
+			return -1;
+		else if(computer == "paper")
+			return 0;
+	}
+	else{
+		if(computer == "rock")
+			return -1;
+		else if(computer == "scissor")
+			return 0;
+		else if(computer == "paper")
+			return 1;
+	}
 }
 
 $(document).ready(function(){
