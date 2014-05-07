f3a46a50f93030e69e5781d10da555351cb41c4f
FanGu <ava.gu1990@gmail.com>
2014-05-06 22:04:09 -0500

    add context and img in modal

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var winCount = 0;
+var loseCount = 0;
 
 function clickImage(choice){
 	// console.log("clickImage excuted!");
@@ -6,17 +8,29 @@ function clickImage(choice){
 	// console.log(number);
 	var computer = ["rock","paper","scissor"];
 	var result = getResult(choice, computer[number]);
+	var resultString = "";
 	if(result == 0){
 		console.log("Tie.");
+		resultString = "Tie";
 	}
 	else if(result == 1){
 		console.log("You win.");
+		resultString = "You win";
+		winCount += 1;
+		$('#ava-win').text("Win : " + winCount);
 	}
 	else{
 		console.log("You lose.");
+		resultString = "You lose";
+		loseCount += 1;
+		$('#ava-lose').text("Lose : " + loseCount);
 	}
 
+	$('#ava-humimg').attr("src",choice+".png");
+	$('#ava-cpuimg').attr("src",computer[number]+".png");
+	$('#result').text(resultString);
 	$('#myModal').modal('show');
+
 }
 
 function getResult(player, computer){
